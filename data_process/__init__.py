9385cd2492c8efe8028a235960f382757393430b
Louis Groux <lgroux@arista.com>
2019-05-30 17:29:29 +0100

    data_process: Fix nominal key usage

--- data_process/__init__.py
+++ data_process/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from collections import deque
+
 def ProcessNotifs(stream, paths=None, keys=None, nominalKeys=None):
     """
     ProcessNotifs consume the batch coming from stream and return them
@@ -15,9 +18,26 @@ def ProcessNotifs(stream, paths=None, keys=None, nominalKeys=None):
             for key, value in notif["updates"]:
                 if keys is not None and key not in keys:
                     continue
+                value = getVal(value, nominalKeys)
                 res = updateDict(res, dname, path, key, nominalKeys, value, time)
     return res
 
+def getVal(nominal, nomKeys):
+    res = nominal
+    if nomKeys is None:
+        return res
+    for k in nomKeys:
+        if k not in res:
+         logging.error(
+"""
+Key  %s not found in json %s
+Full nominal %s
+Nominal key path %s
+""" % (k, res, nominal, nomKeys)
+         )
+         return None
+        res = res[k]
+    return res
 
 def updateDict(resDict, dataset, path, key, nominalKeys, val, ts):
     entry = resDict.setdefault(dataset,
@@ -33,15 +53,22 @@ def SortDict(resDict):
     """
     SortDict orders every timeseries in a hierarchy of dataset by its timestamps.
     """
-    for _, dset in resDict.items():
-        for _, path in dset.items():
-            for _, k in path.items():
-                # grpc.Timestamp cannot be compared, hence the hack.
-                timestamps = [(ts.seconds, ts.nanos) for ts in k["timestamps"]]
-                _, _, k["values"], k["timestamps"] = zip(*sorted(zip(
-                    timestamps,
-                    # In case we have the same timestamp, keep ordering
-                    range(len(timestamps)),
-                    k["values"],
-                    k["timestamps"])))
+
+    def sortTimeSerie(timeSerie):
+        # grpc.Timestamp cannot be compared, hence the hack.
+        timestamps = [(ts.seconds, ts.nanos) for ts in timeSerie["timestamps"]]
+        _, _, timeSerie["values"], k["timestamps"] = zip(*sorted(zip(
+            timestamps,
+            # In case we have the same timestamp, keep ordering
+            range(len(timestamps)),
+            timeSerie["values"],
+            timeSerie["timestamps"])))
+
+    stack = [resDict]
+    while stack:
+        timeSerie = stack.pop()
+        if "values" in timeSerie and "timestamps" in timeSerie:
+            sortTimeSerie(timeSerie)
+        else:
+            map(lambda x: stack.append(x), timeSerie.values())
     return resDict
